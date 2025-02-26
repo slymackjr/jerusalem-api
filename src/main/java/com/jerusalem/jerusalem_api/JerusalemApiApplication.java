726f2e8595b0cd4f08585c35dc3f0fc2076b3ef3
slymackjr <jbnyamasheki@gmail.com>
2025-02-26 07:27:58 +0300

    fix: still no running needs testing more

--- src/main/java/com/jerusalem/jerusalem_api/JerusalemApiApplication.java
+++ src/main/java/com/jerusalem/jerusalem_api/JerusalemApiApplication.java
@@ -2,8 +2,10 @@ package com.jerusalem.jerusalem_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.jerusalem.jerusalem_api"})
+@EnableJpaRepositories(basePackages = "com.jerusalem.jerusalem_api.data.dao")
 public class JerusalemApiApplication {
 
 	public static void main(String[] args) {
